b3a2caff91c4ce45c8d6b13bce85ccb989f202db
Russell Rosario <rosariorussell@gmail.com>
2018-08-19 06:27:52 -0400

    added dynamodb

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ const Api = require('claudia-api-builder')
 const api = new Api()
 
 const getPizzas = require('./handlers/get-pizzas')
+const getOrders = require('./handlers/get-orders')
 const createOrder = require('./handlers/create-order')
 const updateOrder = require('./handlers/update-order')
 const deleteOrder = require('./handlers/delete-order')
@@ -14,8 +15,22 @@ api.get('/', () => 'Welcome to Pizza API')
 api.get('/pizzas', () => {
   return getPizzas()
 })
+
 api.get('/pizzas/{id}', (request) => {
   return getPizzas(request.pathParams.id)
+}, {
+  error: 404
+})
+
+api.get('/orders', () => {
+  return getOrders()
+}, {
+  success: 200,
+  error: 400
+})
+
+api.get('/orders/{id}', (request) => {
+  return getOrders(request.pathParams.id)
 }, {
   error: 404
 })
@@ -26,11 +41,13 @@ api.post('/orders', (request) => {
   success: 201,
   error: 400
 })
+
 api.put('/orders/{id}', (request) => {
   return updateOrder(request.pathParams.id, request.body)
 }, {
   error: 400
 })
+
 api.delete('/orders/{id}', (request) => {
   return deleteOrder(request.pathParams.id)
 }, {
